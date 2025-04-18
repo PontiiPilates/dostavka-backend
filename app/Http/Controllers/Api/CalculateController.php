6547b0a2a73160ccd5bd14315420f7ab5a352e2b
PontiiPilates <s.m.leshukov@gmail.com>
2025-04-18 15:48:22 +0700

    Перемещение контроллера из Web-пространства в пространство Api

--- app/Http/Controllers/Api/CalculateController.php
+++ app/Http/Controllers/Api/CalculateController.php
@@ -1,9 +1,8 @@
 <?php
 
-declare(strict_types=1);
+namespace App\Http\Controllers\Api;
 
-namespace App\Http\Controllers;
-
+use App\Http\Controllers\Controller;
 use App\UseCases\TK\BaikalsrCase;
 use App\UseCases\TK\PochtaCase;
 use Illuminate\Http\Request;
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\CalculateController;
+use App\Http\Controllers\Api\CalculateController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
