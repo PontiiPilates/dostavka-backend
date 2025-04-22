3de1624813d2f5f8f341fd8901d480f842230f0a
PontiiPilates <s.m.leshukov@gmail.com>
2025-04-22 11:01:56 +0700

    Доработка сидера компаний

--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Enums\CompanyType;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -14,8 +15,13 @@ class CompanySeeder extends Seeder
     public function run(): void
     {
         DB::table('companies')->insert([
-            'name' => 'pochta',
-            'label' => 'Почта России',
+            'name' => CompanyType::Pochta->value,
+            'label' => CompanyType::Pochta->label(),
+        ]);
+
+        DB::table('companies')->insert([
+            'name' => CompanyType::Baikal->value,
+            'label' => CompanyType::Baikal->label(),
         ]);
     }
 }
