3b2e99594e684780e1f1a880ab5b176d9dfc4900
PontiiPilates <s.m.leshukov@gmail.com>
2025-04-24 17:28:33 +0700

    Добавление таблицы стран и городов, а также команды для ее наполнения

---
+++ app/Models/City.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class City extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'city_id',
+        'country_code',
+        'country_name',
+        'country_fullname',
+        'region_code',
+        'region_name',
+        'city_code',
+        'city_name',
+        'index_min',
+        'index_max',
+        'alpha2',
+        'alpha3',
+    ];
+}
