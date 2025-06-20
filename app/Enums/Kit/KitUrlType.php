eaf389e02fbdf6c7fa1be25243f3f0e632a02682
PontiiPilates <s.m.leshukov@gmail.com>
2025-06-20 09:43:22 +0700

    Правки

--- app/Enums/Kit/KitUrlType.php
+++ app/Enums/Kit/KitUrlType.php
@@ -8,6 +8,7 @@ enum KitUrlType: string
 {
     case City = '/1.1/tdd/city/get-list';
     case Calculate = '/1.1/order/calculate';
+    case Regions = '/1.0/tdd/region/get-list';
     // case Type = '/calculator/PriceTypeListAvailable';
 
     public function label()
@@ -15,6 +16,7 @@ enum KitUrlType: string
         return match ($this) {
             self::City => 'Список городов',
             self::Calculate => 'Расчёт стоимости доставки',
+            self::Regions => 'Список регионов',
             // self::Type => 'Способы доставки',
         };
     }
