12bb1e3f3ab3da17bf089325f72e409a098fcda0
PontiiPilates <s.m.leshukov@gmail.com>
2025-06-20 13:29:19 +0700

    Добавление теста для проверки гео-метода API ТК

---
+++ tests/Feature/TK/BaikalApiTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature\TK;
+
+use App\Enums\Baikal\BaikalUrlType;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Http;
+use Tests\TestCase;
+
+class BaikalApiTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_cities()
+    {
+        $username = config('companies.baikal.username');
+        $url = config('companies.baikal.url') . BaikalUrlType::Affiliate->value;
+
+        $response = Http::withBasicAuth($username, '')->get($url);
+
+        $this->assertEquals(200, $response->status());
+        $this->assertIsArray($response->json());
+        // $this->assertArrayHasKey('cityList', $response->json());
+    }
+}
