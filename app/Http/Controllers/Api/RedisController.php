e8f82be832e497740a14458e479064fdbb9d99e5
Sergey Leshukov <s.m.leshukov@gmail.com>
2025-08-25 15:01:30 +0700

    feat: добавление маршрутов для redis

---
+++ app/Http/Controllers/Api/RedisController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
+
+class RedisController extends Controller
+{
+    /**
+     * Возвращает конкретный результат по ключу
+     */
+    public function handle(Request $request)
+    {
+        $get = json_decode(Redis::get($request->get), true);
+
+        return response()->json($get);
+    }
+
+    /**
+     * Возвращает все записи
+     */
+    public function viewAll()
+    {
+        $all = Redis::keys('*');
+
+        return response()->json($all);
+    }
+
+    /**
+     * Очищает от всех записей
+     */
+    public function clear()
+    {
+        $all = Redis::keys('*');
+
+        foreach ($all as $key) {
+            Redis::del($key);
+            return response()->json('Deleted', 200);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\AutocompleteController;
 use App\Http\Controllers\Api\CalculateController;
+use App\Http\Controllers\Api\RedisController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,6 +22,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::prefix('v1')->group(function () {
-    Route::get('/calculate', [CalculateController::class, 'handle'])->name('calculate');
     Route::get('/autocomplete', [AutocompleteController::class, 'handle'])->name('autocomplete');
+
+    Route::get('/calculate', [CalculateController::class, 'handle'])->name('calculate');
+
+    Route::get('/calculate-result', [RedisController::class, 'handle'])->name('calculate-result');
+    Route::get('/view-all', [RedisController::class, 'viewAll'])->name('view-all');
+    Route::get('/clear', [RedisController::class, 'clear'])->name('clear');
 });
